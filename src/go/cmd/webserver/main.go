8c5e5975c53b300fb2ac0f29e34a9adda73da3a5
Uk1d <2577552541@qq.com>
2026-03-25 00:08:50 +0800

    chore: add Apache License 2.0 and copyright headers
    - Add LICENSE file with Apache 2.0 full text
    - Add copyright headers to all C and Go source files

--- src/go/cmd/webserver/main.go
+++ src/go/cmd/webserver/main.go
@@ -1,3 +1,18 @@
+// Copyright 2026 Uk1d
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
 // Package main 提供 Web 服务独立入口
 // 该服务可运行在普通用户权限下，通过 WebSocket 连接监控程序获取数据
 package main
